42d04e42353adeedce1baf1ef8f717bdc839a52a
danay <danidagosudarev@gmail.com>
2023-03-02 15:05:08 +0300

    add if else for and do while

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+let numberOfFilms
+
+do {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+} while (numberOfFilms == false);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +14,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < numberOfFilms; i++) {
+    let a, b;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    do {
+        a = prompt('Один из просмотренных фильмов?', '');
+    } while ((a.length > 50) || (a === '') || (a == null));
+
+    do {
+        b = prompt('На сколько оцените его?', '');
+    } while ((b.length > 50) || (b === '') || (b == null));
+
+    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
